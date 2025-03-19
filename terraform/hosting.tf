9812986fa3874d940f023c807d055157ac22220c
Mihai <mihaimisai@gmail.com>
2025-03-19 16:16:48 +0000

    removed unused files

--- terraform/hosting.tf
+++
@@ -1,5 +0,0 @@
-resource "google_storage_bucket" "react_app_bucket" {
-  name     = "shopping-list-react"
-  location = "EU"
-  storage_class = "STANDARD"
-}
--- terraform/main.tf
+++
@@ -1,5 +0,0 @@
-
-provider "google" {
-
-  region = "europe-west2"
-}
