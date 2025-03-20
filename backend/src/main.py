68b4f2ded50e19d03b8bdb139b957afdf5a6b4b0
Mihai <mihaimisai@gmail.com>
2025-03-20 16:31:32 +0000

    added dev https paths to cors origins

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,8 +5,9 @@ app = FastAPI()
 
 origins = [
     "http://localhost:3000",
-    "https://simple-shopping-gcp.web.app",
-    "https://simple-shopping-gcp.firebaseapp.com"
+    "https://simple-shopping-gcp.web.app", #Live 1
+    "https://simple-shopping-gcp.firebaseapp.com" #Live 2
+    "https://simple-shopping-gcp--dev-w2v6xn68.web.app", #Dev 
 ]
 
 app.add_middleware(
