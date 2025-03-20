6575cccdc5cab1e112f2026f6a24b60adbfb6ec4
Mihai <mihaimisai@gmail.com>
2025-03-20 16:02:22 +0000

    added test fetch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,30 @@
-
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [message, setMessage] = useState('')
+
+  useEffect( () => {
+    
+    const fastApi = 'https://shopping-list-fastapi-94310770586.europe-west2.run.app'
+
+    fetch(fastApi)
+      .then(response => {
+        if(response.ok) {
+          console.log(response.json())
+        }
+      })
+
+
+
+  }, [])
+
+
   return (
-    <h1> HELLO </h1>
+    <div>
+      <h1> HELLO </h1>
+      <p>The response from FastAPI is: {message}</p>
+    </div>
   );
 }
 
