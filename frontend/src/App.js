74b00f9a429ae00108287e6329550d81e4548727
Mihai <mihaimisai@gmail.com>
2025-03-27 07:47:12 +0000

    added routes, authprovider and navigation bar area

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import Header from "./components/Header"
+import { AuthProvider } from '../../../task_manager/frontend/src/contexts/AuthContext';
+import LoginApp from './components/LoginApp';
 
 function App() {
 
@@ -25,14 +28,30 @@ function App() {
   }, [])
 
 
-  return (
-    <div className="App">
-      <div className="row d-flex text-center my-5 justify-content-center">
-      <Header />
-        <h1> HELLO </h1>
-        <p>The response from FastAPI is: {message}</p>
-      </div>
-    </div>
+  return ( <>
+     <BrowserRouter>
+      <AuthProvider>
+        <div className="Nav">
+          {/* Navigation menu & hamburger */}
+          <div className="col-2 d-flex flex-column align-items-center text-center">
+            <h3>Navigation</h3>
+          </div>
+        </div>
+        
+        <div className="row d-flex text-center my-5 justify-content-center">
+          <Header />
+          <h1> HELLO </h1>
+          <p>The response from FastAPI is: {message}</p>
+          <LoginApp />
+          <Routes>
+          </Routes>
+        </div>
+  
+      </AuthProvider>
+     </BrowserRouter>
+
+    </>
+    
   );
 }
 
