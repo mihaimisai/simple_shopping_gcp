77bc99e84a6ec7b8765ea1f7f82ad28781b55f52
Mihai <mihaimisai@gmail.com>
2025-03-31 11:14:31 +0100

    added firebase-config, added secrets for credentials, created a signup function

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -1,21 +1,45 @@
 
-import React, { useContext}  from "react";
+import React, { createContext, useContext, useState, useEffect}  from "react";
+import app from "../firebase-config"
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    onAuthStateChanged
+} from "firebase/auth"
 
+const AuthContext = createContext();
 
-const AuthContext = React.createContext()
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
 
-export function useAuth() {
-    return useContext(AuthContext)
-}
+export const AuthProvider = ({ children }) => {
 
-export function AuthProvider({children}) {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const auth = getAuth(app);
+
+  const signup = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe; // Cleanup subscription on unmount
+  }, [auth]);
 
     const value = {
+        signup
     }
 
-    return(
+    return (
         <AuthContext.Provider value={value}>
-            {children}
+          {!loading && children}
         </AuthContext.Provider>
-    )
-}
+      );
+};
