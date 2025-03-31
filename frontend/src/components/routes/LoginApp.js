09aa50c23b8ee6c0e0a1474869d312aec24c58a8
Mihai <mihaimisai@gmail.com>
2025-03-31 10:37:11 +0100

    login app styled and js script added

--- frontend/src/components/routes/LoginApp.js
+++ frontend/src/components/routes/LoginApp.js
@@ -9,26 +9,27 @@ function LoginApp() {
 
     return (
         
-        <div>
+        <div className="d-flex flex-column align-items-center justify-content-center">
 
-            <button className="btn btn-warning" onClick={() => setShowLogin(!showLogin)}>
-                { showLogin ? 'Switch to Signup' : 'Switch to Login' }
-            </button>
-            
-            {showLogin ? 
-                <LoginForm setMessage={setMessage}/>
-                :
-                <SignUpForm setMessage={setMessage}/>        
-            }
-            
+            <div className="my-4">
+                <button className="btn btn-warning" onClick={() => setShowLogin(!showLogin)}>
+                    { showLogin ? 'Switch to Signup' : 'Switch to Login' }
+                </button>
+            </div>
+
             <div>
-              <div>
+                {showLogin ? 
+                    <LoginForm setMessage={setMessage}/>
+                    :
+                    <SignUpForm setMessage={setMessage}/>        
+                }
+            </div>
+
+            <div className="my-4">
                 {message && <div className='alert alert-danger mt-5 col-4'>{message}</div>}
                 <p>Before using this app, you need to sign in.</p>
                 <p className="text-info">For testing purposes you can use email: test@test.com and password: 123456</p>
-              </div>
             </div>
-
 
         </div>
     )
