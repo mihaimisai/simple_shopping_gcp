eee77bce476787dffcf8d94666e4c349a2e6089d
Mihai <mihaimisai@gmail.com>
2025-04-23 17:10:57 +0100

    simplified db connection

--- backend/src/firebase_utils.py
+++ backend/src/firebase_utils.py
@@ -1,28 +1,11 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-import os
-import json
+from google.cloud import firestore
 import logging
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
-# Load the service account key from the environment variable
-service_account_json = os.environ.get(
-    "FIREBASE_SERVICE_ACCOUNT_SIMPLE_SHOPPING_GCP"
-)  # noqa
-
-db = None
-
-if service_account_json:
-    try:
-        service_account_info = json.loads(service_account_json)
-        cred = credentials.Certificate(service_account_info)
-        firebase_admin.initialize_app(cred)
-        db = firestore.client()
-        logging.info("Connected to Firestore")
-
-    except (ValueError, KeyError, TypeError) as e:
-        logging.error(f"Error connecting to Firestore: {e}")
-else:
-    logging.error("Service account key environment variable not found.")
+try:
+    db = firestore.Client()
+    logging.info("Connected to Firestore")
+except Exception as e:
+    logging.error(f"Error connecting to Firestore: {e}")
